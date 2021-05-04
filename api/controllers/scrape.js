9ce17694a9b214e0c3e3aae56660c03f0d94a045
hussain2510 <mdhussainahmad993@gmail.com>
2021-05-05 01:39:10 +0530

    lastupdate

--- api/controllers/scrape.js
+++ api/controllers/scrape.js
@@ -90,7 +90,7 @@ var arguments=process.argv;
     
 
     async function configureBrowser(url){
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     // await page.goto(url);
     //  await page.waitFor('.bot-list-section')
